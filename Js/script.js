0ec9d56be5373805f3d4be573a7c0539ea740a11
Ali <aliimottaghiii@gmail.com>
2023-09-06 19:19:54 +0330

    preloader added

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,11 @@
 let listItems = document.querySelectorAll('ul .items');
+
 console.log(listItems)
 for(let item of listItems) {
     item.addEventListener('click' , (ev) => {
         document.querySelector('ul .selected').classList.remove('selected');
         ev.target.classList.add('selected');
-
+        
         let textTarget = ev.target.getAttribute('data-list');
         document.querySelector('section .selected').classList.remove('selected');
         document.querySelector(`section div[data-list="${textTarget}"]`).classList.add('selected');
